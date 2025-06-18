e1e3df2e9112654e1eda411f88f3c5a277822e96
BVK Ratnesh <ratneshbvk2504@gmail.com>
2025-06-18 21:20:47 +0530

    Edit Task

--- client/src/components/ListTodos.jsx
+++ client/src/components/ListTodos.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import EditTodo from "./EditTodo";
 
 function ListTodos() {
   const [todos, setTodos] = useState([]);
@@ -42,7 +43,9 @@ function ListTodos() {
           {todos.map((todo) => (
             <tr key={todo.todo_id}>
               <td>{todo.description}</td>
-              <td>Edit</td>
+              <td>
+                <EditTodo todo={todo} />
+              </td>
               <td>
                 <button
                   className="btn btn-danger"
